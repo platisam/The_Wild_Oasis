4f088eb23f1a78e230d7d518f0edd35c79f9e93c
platisam <platisam@gmail.com>
2023-08-06 00:04:25 +0200

    adding css variables to styled-components elements

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,10 @@ const Button = styled.button`
   padding: 1.2rem 1.6rem;
   font-weight: 500;
   border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-brand-600);
+  color: var(--color-brand-50);
+  box-shadow: var(--shadow-sm);
   cursor: pointer;
   margin: 20px;
 `;
