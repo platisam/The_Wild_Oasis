68d267a7e72a92b598f423b37a68659660708513
platisam <platisam@gmail.com>
2023-08-06 22:07:15 +0200

    styling Sidebar component

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,7 +1,15 @@
+import styled from "styled-components";
+
+const StyledSidebar = styled.aside`
+  background-color: blue;
+  padding: 3.2rem 2.4rem;
+  border-right: 1px solid var(--color-grey-100);
+`;
+
 const Sidebar = () => {
   return (
     <div>
-      <aside>SIDEBAR</aside>
+      <StyledSidebar>SIDEBAR</StyledSidebar>
     </div>
   );
 };
