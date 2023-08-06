5a04b5251c49a7a22a496f789734fc534034701a
platisam <platisam@gmail.com>
2023-08-06 15:00:05 +0200

    styling Row component

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Heading from "./ui/Heading";
 import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: orangered;
+  /*background-color: orangered;*/
   padding: 20px;
 `;
 
@@ -15,21 +15,23 @@ const App = () => {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Row type="horizontal">
-          <Heading as="h1">The Wild Oasis</Heading>
-          <div>
-            <Heading as="h2">Check in and out</Heading>
-            <Button onClick={() => alert("check in")}>Check in</Button>
-            <Button onClick={() => alert("check out")}>Check out</Button>
-          </div>
-        </Row>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("check in")}>Check in</Button>
+              <Button onClick={() => alert("check out")}>Check out</Button>
+            </div>
+          </Row>
 
-        <Row type="vertical">
-          <Heading as="h3">Form</Heading>
-          <form>
-            <Input type="number" placeholder="Number of guests" />
-            <Input type="number" placeholder="Number of guests" />
-          </form>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
         </Row>
       </StyledApp>
     </>
