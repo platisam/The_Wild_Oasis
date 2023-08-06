543a5f6ac06d1e69a630ea61ea57b39ae7d50afc
platisam <platisam@gmail.com>
2023-08-06 22:36:15 +0200

    styling AppLayout component

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,16 +1,14 @@
 import styled from "styled-components";
 
 const StyledSidebar = styled.aside`
-  background-color: blue;
+  background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
   border-right: 1px solid var(--color-grey-100);
+
+  grid-row: 1 / -1;
 `;
 
 const Sidebar = () => {
-  return (
-    <div>
-      <StyledSidebar>SIDEBAR</StyledSidebar>
-    </div>
-  );
+  return <StyledSidebar>SIDEBAR</StyledSidebar>;
 };
 export default Sidebar;
