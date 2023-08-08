79b22daf7777ba7f1d6d7a5bbf3cf0bab3d5cd42
platisam <platisam@gmail.com>
2023-08-08 15:20:18 +0200

    fetching data from cabin using useQuery custom hook

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
+import { getCabins } from "../../services/apiCabins";
+import Spinner from "../../ui/Spinner";
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -25,6 +28,17 @@ const TableHeader = styled.header`
 `;
 
 const CabinTable = () => {
+  const {
+    isLoading,
+    data: cabins,
+    error,
+  } = useQuery({
+    queryKey: ["cabin"],
+    queryFn: getCabins,
+  });
+
+  if (isLoading) return <Spinner />;
+
   return <div>Table</div>;
 };
 export default CabinTable;
