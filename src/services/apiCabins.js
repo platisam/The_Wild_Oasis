33a6ceccbee0eeced15b32c2e42523d3519919ef
platisam <platisam@gmail.com>
2023-08-09 17:54:18 +0200

    creating imagePath using supabaseUrl and uploading image

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -17,10 +17,14 @@ export async function createCabin(newCabin) {
     ""
   );
 
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
   // https://upfajpmdrqtipqqhefxd.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg
 
   // 1. Create cabin
-  const { data, error } = await supabase.from("cabins").insert([newCabin]);
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([{ ...newCabin, image: imagePath }]);
 
   if (error) {
     console.error(error);
@@ -28,6 +32,18 @@ export async function createCabin(newCabin) {
   }
 
   // 2. Upload image
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  // 3. Delete the cabin if there was an error uploading image
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.error(storageError);
+    throw new Error(
+      "Cabin image could not be uploaded and the cabin was not created"
+    );
+  }
 
   return data;
 }
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://upfajpmdrqtipqqhefxd.supabase.co";
+export const supabaseUrl = "https://upfajpmdrqtipqqhefxd.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVwZmFqcG1kcnF0aXBxcWhlZnhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEzOTU5NzEsImV4cCI6MjAwNjk3MTk3MX0.JiIcRuVva8rL0dIJ5kpCRd4sslGKJjGAHXyKaTtZZjA";
 const supabase = createClient(supabaseUrl, supabaseKey);
