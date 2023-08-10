d902bcd5c028f7762adf04f9a4b6c589d6563e99
platisam <platisam@gmail.com>
2023-08-10 23:25:26 +0200

    using useCabins custom hook in CabinTable component

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,8 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
-import { getCabins } from "../../services/apiCabins";
+
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
+import { useCabins } from "./useCabins";
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -29,6 +29,8 @@ const TableHeader = styled.header`
 `;
 
 const CabinTable = () => {
+  const { isLoading, cabins } = useCabins();
+
   if (isLoading) return <Spinner />;
 
   return (
