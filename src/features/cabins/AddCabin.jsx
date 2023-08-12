21607baeebab66a0d2aeb38b7303a234726e7c8d
platisam <platisam@gmail.com>
2023-08-12 22:46:59 +0200

    remodeling AddCabin component

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -3,6 +3,26 @@ import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
 
+function AddCabin() {
+  return (
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      {/*<Modal.Open opens="table">
+        <Button>Show table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CreateCabinForm />
+  </Modal.Window>*/}
+    </Modal>
+  );
+}
+
 {
   /*const AddCabin = () => {
   const [isOpenModal, setIsOpenModal] = useState(false);
