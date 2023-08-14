fb2163620f203219e164a8eb79de493ef3b3afc4
platisam <platisam@gmail.com>
2023-08-14 16:27:40 +0200

    creating CabinTableOperations component and adding to Cabins

---
+++ src/features/cabins/CabinTableOperations.jsx
@@ -0,0 +1,11 @@
+import TableOperations from "../../ui/TableOperations.jsx";
+import Filter from "../../ui/Filter.jsx";
+
+const CabinTableOperations = () => {
+  return (
+    <TableOperations>
+      <Filter />
+    </TableOperations>
+  );
+};
+export default CabinTableOperations;
