3edb179f0dd3ffd4ec423a610e4bf0abb6cf66f0
platisam <platisam@gmail.com>
2023-08-16 11:44:04 +0200

    completing pagination functionality

--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -81,18 +81,26 @@ const Pagination = ({ count }) => {
     setSearchParams(searchParams);
   }
 
+  if (pageCount <= 1) return null;
+
   return (
     <StyledPagination>
       <P>
-        Showing <span>1</span> to <span>10</span> of <span>{count}</span>{" "}
-        results
+        Showing <span>{(currentPage - 1) * PAGE_SIZE + 1}</span> to{" "}
+        <span>
+          {currentPage === pageCount ? count : currentPage * PAGE_SIZE}
+        </span>{" "}
+        of <span>{count}</span> results
       </P>
 
       <Buttons>
-        <PaginationButton onClick={prevPage}>
+        <PaginationButton onClick={prevPage} disabled={currentPage === 1}>
           <HiChevronLeft /> <span>Previous</span>
         </PaginationButton>
-        <PaginationButton onClick={nextPage}>
+        <PaginationButton
+          onClick={nextPage}
+          disabled={currentPage === pageCount}
+        >
           <span>Next</span>
           <HiChevronRight />
         </PaginationButton>
