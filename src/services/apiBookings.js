68b2c7d628f20c2aa4646e7cf3a8f4ae60c07584
platisam <platisam@gmail.com>
2023-08-16 13:45:21 +0200

    completing paginating bookings

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,5 +1,6 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
+import { PAGE_SIZE } from "../utils/constants.js";
 
 export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
@@ -17,6 +18,12 @@ export async function getBookings({ filter, sortBy, page }) {
     query = query.order(sortBy.field, {
       ascending: sortBy.direction === "asc",
     });
+
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
+    query = query.range(from, to);
+  }
 
   const { data, error, count } = await query;
 
