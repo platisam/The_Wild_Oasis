1a541293754d287fb999ee4d1069fbb90152a479
platisam <platisam@gmail.com>
2023-08-18 12:43:28 +0200

    adding ProtectedRoute component

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+const ProtectedRoute = ({ children }) => {
+  // 1. Load the authenticated user
+
+  // 2. While loading, show a spinner
+
+  // 3. If there is NO authenticated user, redirect to the /login
+
+  // 4. If there IS a user, render the app
+  return children;
+};
+export default ProtectedRoute;
