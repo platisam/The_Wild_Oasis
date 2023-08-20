3b96d96b1ebf07f36ce1cf7385cb82febfe43411
platisam <platisam@gmail.com>
2023-08-20 22:22:20 +0200

    creating context folder and DarkModeContext component

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+import { useLocalStorageState } from "../hooks/useLocalStorageState.js";
+
+const DarkModeContext = createContext();
+
+function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDark) => !isDark);
+  }
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
