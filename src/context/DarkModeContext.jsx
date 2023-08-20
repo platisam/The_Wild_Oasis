f6940143efcca074753337411d4675e2a7b7b3e5
platisam <platisam@gmail.com>
2023-08-20 23:00:23 +0200

    final adding dark mode functionality to ui using useeffect hook

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
 import { useLocalStorageState } from "../hooks/useLocalStorageState.js";
 
 const DarkModeContext = createContext();
@@ -9,6 +9,16 @@ function DarkModeProvider({ children }) {
   function toggleDarkMode() {
     setIsDarkMode((isDark) => !isDark);
   }
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark-mode");
+      document.documentElement.classList.remove("light-mode");
+    } else {
+      document.documentElement.classList.add("light-mode");
+      document.documentElement.classList.remove("dark-mode");
+    }
+  }, [isDarkMode]);
 
   return (
     <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
