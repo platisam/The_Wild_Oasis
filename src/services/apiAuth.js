c1640f233fee46525badecedfb8193dcad753d05
platisam <platisam@gmail.com>
2023-08-20 20:00:34 +0200

    building updateCurrentUser function using supabase

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -42,3 +42,26 @@ export async function logout() {
   const { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
 }
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  // 1. Update password OR fullName
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+
+  const { data, error } = await supabase.auth.updateUser(updateData);
+
+  if (error) throw new Error(error.message);
+  if (!avatar) return data;
+
+  // 2. Upload the avatar image
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+
+  if (storageError) throw new Error(error.message);
+
+  // 3. Update avatar in the user
+}
