73f90bbd3ed4214bb86a80abf391b84d01a996aa
platisam <platisam@gmail.com>
2023-08-20 21:11:40 +0200

    completion of user data update

--- src/features/authentication/useUpdateUser.js
+++ src/features/authentication/useUpdateUser.js
@@ -1,5 +1,4 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-
 import { toast } from "react-hot-toast";
 import { updateCurrentUser } from "../../services/apiAuth";
 
@@ -8,8 +7,9 @@ export function useUpdateUser() {
 
   const { mutate: updateUser, isLoading: isUpdating } = useMutation({
     mutationFn: updateCurrentUser,
-    onSuccess: () => {
+    onSuccess: ({ user }) => {
       toast.success("User account successfully updated");
+      // queryClient.setQueryData("user", user);
       queryClient.invalidateQueries({ queryKey: ["user"] });
     },
     onError: (err) => toast.error(err.message),
