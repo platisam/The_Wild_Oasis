e5951a98debfc0c925d1f560585c37f66ab695f7
platisam <platisam@gmail.com>
2023-08-20 21:58:32 +0200

    creating and adding DarkModeToggle component to HeaderMenu

---
+++ src/ui/DarkModeToggle.jsx
@@ -0,0 +1,11 @@
+import { HiOutlineMoon } from "react-icons/hi2";
+import ButtonIcon from "./ButtonIcon";
+
+const DarkModeToggle = () => {
+  return (
+    <ButtonIcon>
+      <HiOutlineMoon />
+    </ButtonIcon>
+  );
+};
+export default DarkModeToggle;
